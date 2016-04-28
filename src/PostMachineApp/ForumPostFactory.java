aefcc0b87fa291042ccf47a9fca03bcc1142a655
vhuang1 <vhuang1@DSGDQTTG82.prod.quest.corp>
2016-04-28 14:23:36 +0800

    optimize console output

--- src/PostMachineApp/ForumPostFactory.java
+++ src/PostMachineApp/ForumPostFactory.java
@@ -10,13 +10,16 @@ package PostMachineApp;
  * @author MyTest
  */
 public class ForumPostFactory {
-    public static ForumPost CreateForumPost(Boolean EnableThread,Integer ThreadID,String FirefoxPath, String Profile,String PostEntity,long StartTime,Boolean EnableStopTime,long StopTime,Integer RefreshPostCount,long PostCount,Integer FixedWaitTime,Integer RandomWaitTime,String PostUrl,String PostContent){
-    if (PostEntity.equals("Vivo")) {
-			return new VivoForumPost(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, PostUrl, PostContent);
-		} else if  (PostEntity.equals("Oppo")) {
-			return new OppoForumPost(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, PostUrl, PostContent);
-		}
-		return null;
+
+    public static ForumPost CreateForumPost(Boolean EnableThread, Integer ThreadID, String FirefoxPath, String Profile, String PostEntity, long StartTime, Boolean EnableStopTime, long StopTime, Integer RefreshPostCount, long PostCount, Integer FixedWaitTime, Integer RandomWaitTime, String PostUrl, String PostContent) {
+        if (PostEntity.equals("Vivo")) {
+            return new VivoForumPost(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, PostUrl, PostContent);
+        } else if (PostEntity.equals("Oppo")) {
+            return new OppoForumPost(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, PostUrl, PostContent);
+        } else if (PostEntity.equals("VivoMobile")) {
+            return new VivoMobileForumPost(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, PostUrl, PostContent);
+        }
+        return null;
     }
-    
+
 }
