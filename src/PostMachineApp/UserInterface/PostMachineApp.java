282fe20b0e03153a492acacd4b9e68d08dce7756
Administrator <Administrator@NB2K12>
2016-07-22 07:53:34 +0800

    增加Send Post Step UI

--- src/PostMachineApp/UserInterface/PostMachineApp.java
+++ src/PostMachineApp/UserInterface/PostMachineApp.java
@@ -787,13 +787,13 @@ public class PostMachineApp extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "StepID", "TaskID", "PProerty", "PPropertyValue", "CProperty", "CPropertyValue", "Action", "Data"
             }
         ));
         jScrollPane1.setViewportView(jTable1);
