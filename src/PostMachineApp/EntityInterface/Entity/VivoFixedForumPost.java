caba53da772422b075d54ffb952a9430be295809
Administrator <Administrator@NB2K12>
2016-09-26 05:20:53 +0800

    修改vivo fixed bug

--- src/PostMachineApp/EntityInterface/Entity/VivoFixedForumPost.java
+++ src/PostMachineApp/EntityInterface/Entity/VivoFixedForumPost.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-final public class VivoFixedForumPost extends MeizuForumPost {
+ public class VivoFixedForumPost extends VivoForumPost {
 
     public VivoFixedForumPost(Boolean EnableThread, Integer ThreadID, String FirefoxPath, String Profile, String PostEntity, long StartTime, Boolean EnableStopTime, long StopTime, Integer RefreshPostCount, long PostCount, Integer FixedWaitTime, Integer RandomWaitTime, Integer RestWaitTime, Integer RestWaitPostCount,Integer RestWaitPostCountOffset, String PostUrl, String PostContent) {
         super(EnableThread, ThreadID, FirefoxPath, Profile, PostEntity, StartTime, EnableStopTime, StopTime, RefreshPostCount, PostCount, FixedWaitTime, RandomWaitTime, RestWaitTime, RestWaitPostCount,RestWaitPostCountOffset, PostUrl, PostContent);
